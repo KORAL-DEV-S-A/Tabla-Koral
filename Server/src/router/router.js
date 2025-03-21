9a946b627a809e91a5f1219c1819d45c2c991053
Alexis Coronel <alexiscoronel545@gmail.com>
2025-03-21 00:35:56 -0300

    Squashed commit of the following:
    commit f55b2250f7e523d250707a1c54f38a3986d47a8f
    Merge: c31c29b 76da1a0
    Author: Alexis Coronel <alexiscoronel545@gmail.com>
    Date:   Fri Mar 21 00:32:26 2025 -0300
    
        Merge pull request #6 from KORAL-DEV-S-A/3-create-server-with-express
    
        Intregration of socket complete in Server with express
    
    commit 76da1a07c83ff0d7fc08ad4a7486c57e708bbba4
    Author: Agustin Kowalczuk <Kowalczukagustin@gmail.com>
    Date:   Fri Mar 21 00:31:16 2025 -0300
    
        Intregration of socket complete in Server with express

--- Server/src/router/router.js
+++ Server/src/router/router.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
+import {io} from '../app.js';
 
 // Router
 const router_index = Router();
 
 // Routes
 router_index.get('/', (req, res) => {
+    io.emit('welcome', `Hello World ${io.id}`);
     res.send('Hello World');
 });
 
